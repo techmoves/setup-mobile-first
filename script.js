5dd849a71cdf9bafab469f87fc37c22138207c43
techmoves <muhndlovu28@gmail.com>
2023-04-22 01:03:58 +0200

    Update the pop up

--- script.js
+++ script.js
@@ -81,48 +81,45 @@ form.addEventListener('submit', (event) => {
 // ---------------------popUp section-----------------------
 const cardData = [
   {
-    heading: 'Data Dashboard ',
+    heading: "Data Dashboard ",
+    heading3: "Keeping track of hundreds  of components website",
     text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    lists: ['html', 'bootstrap', 'ruby'],
-    btn: 'see project',
-
+    lists: ["html", "bootstrap", "ruby"],
+    btn: "see project",
   },
   {
-    heading: 'Data Dashboard ',
+    heading: "Data Dashboard ",
     text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    lists: ['html', 'bootstrap', 'ruby'],
-    btn: 'see project',
-
+    lists: ["html", "bootstrap", "ruby"],
+    btn: "see project",
   },
   {
-    heading: 'Data Dashboard ',
-    heading2: 'Website Portfolio',
+    heading: "Data Dashboard ",
+    heading2: "Website Portfolio",
     text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    lists: ['html', 'bootstrap', 'ruby'],
-    btn: 'see project',
-
+    lists: ["html", "bootstrap", "ruby"],
+    btn: "see project",
   },
   {
-    heading: 'Data Dashboard ',
+    heading: "Data Dashboard ",
     text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    lists: ['html', 'bootstrap', 'ruby'],
-    btn: 'see project',
-
+    lists: ["html", "bootstrap", "ruby"],
+    btn: "see project",
   },
   {
-    heading: 'Data Dashboard ',
+    heading: "Data Dashboard ",
+    heading3: "Keeping track of hundreds  of components website",
     text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    lists: ['html', 'bootstrap', 'ruby'],
-    btn: 'see project',
-
+    lists: ["html", "bootstrap", "ruby"],
+    btn: "see project",
   },
   {
-    heading: 'Data Dashboard ',
-    heading2: 'Website Portfolio',
+    heading: "Data Dashboard ",
+    heading3: "Keeping track of hundreds  of components website",
+    heading2: "Website Portfolio",
     text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
-    lists: ['html', 'bootstrap', 'ruby'],
-    btn: 'see project',
-
+    lists: ["html", "bootstrap", "ruby"],
+    btn: "see project",
   },
 ];
 
@@ -195,7 +192,10 @@ btnClick.forEach((btn) => {
     pop.innerHTML = `
     <div id="top-popup">
     <h3 class="top-stories">${cardData[x].heading} </h3>
+    <h3 class="top-stories top-hd">${cardData[x].heading3} </h3>
+<h3 class="top-hd">Keeping track of hundreds  of components website</h3>
     <button class="close-button">&times;</button>
+    
     <div class="programs">
         <div class="position-buttons">
             <button class="recentBtn" type="button" id="color">html </button>
@@ -203,15 +203,22 @@ btnClick.forEach((btn) => {
             <button class="recentBtn" type="button" id="color">Ruby</button>
         </div>
     </div>
-   </div>                    
+   </div> 
+<div class="destop5">
 <div class="popup-photo">
 <img class="recent-pic" src="images/snapshoot.png" alt="snapshoot Portfolio">
 </div>
-<div class="popup-para">
-<span class="recent-para">
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
-</span>
-</div>
+  
+    <div class="popup-para">
+    <span class="recent-para1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent </>
+    </span>
+    <span class="recent-para2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+    </span>
+    </div>
+  </div> 
+
 <div class="btn">
 <button type="button" class="get-my-resume-btn">See live <a href="#"><img src="images/Icon2g.png" alt="GitHub" class="github"> </button>
 <button type="button" class="get-my-resume-btn">See source  <a href="#"><img src="images/github.png" alt="GitHub" class="github"></a> </button>
