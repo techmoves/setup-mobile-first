d9b1c580af12b87a4a67a08e9318e296f71be105
techmoves <muhndlovu28@gmail.com>
2023-04-21 22:22:50 +0200

    updated popup and it styling

--- script.js
+++ script.js
@@ -77,3 +77,157 @@ form.addEventListener('submit', (event) => {
     errorMessage.textContent = '';
   }
 });
+
+//---------------------popUp section-----------------------
+    const cardData = [
+      {
+        heading: 'Data Dashboard ',
+        text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        lists: ['html','bootstrap','ruby'],
+        btn: 'see project'
+
+      },
+      {
+        heading: 'Data Dashboard ',
+        text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        lists: ['html','bootstrap','ruby'],
+        btn: 'see project'
+
+      },
+      {
+        heading: 'Data Dashboard ',
+        heading2 : 'Website Portfolio',
+        text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        lists: ['html','bootstrap','ruby'],
+        btn: 'see project'
+
+      },
+      {
+        heading: 'Data Dashboard ',
+        text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        lists: ['html','bootstrap','ruby'],
+        btn: 'see project'
+
+      },
+      {
+        heading: 'Data Dashboard ',
+        text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        lists:  ['html','bootstrap','ruby'],
+        btn: 'see project'
+
+      },
+      {
+        heading: 'Data Dashboard ',
+        heading2 : 'Website Portfolio',
+        text: "selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+        lists:  ['html','bootstrap','ruby'],      
+        btn: 'see project'
+
+      },
+    ]
+
+const work = document.getElementById('test');
+
+cardData.forEach((value,index) => {
+  const div = document.createElement('div');
+  div.className = "details";
+  if(index === 0 || index === 3 ){
+    div.id = index;
+    div.innerHTML = `
+    <h2 class="card1-head">${value.heading}</h2>
+    <p class="data-arc">${value.text}</p>
+    <div class="programs programs1">
+        <ul class="position-items">
+            <li class="stack-position">html </li>
+            <li class="stack-position">bootstrap </li>
+            <li class="stack-position">Ruby</li>
+        </ul>
+    </div>
+     <button type="button" class="see-project-btn button-hov hov" id="1">See Project </button>
+    `
+  }else if (index === 1 || index === 4) {
+    div.className = "details1";
+    div.id = index;
+    div.innerHTML = `
+    <h2 class="card1-head">${value.heading}</h2>
+    <p class="data-arc">${value.text}</p>
+    <div class="programs">
+        <ul class="position-items">
+            <li class="stack-position">html </li>
+            <li class="stack-position">bootstrap </li>
+            <li class="stack-position">Ruby</li>
+        </ul> 
+    </div>
+    <button type="button" class="see-project-btn button-hov hov">See Project </button>
+    `
+  }else if (index === 2 || index === 5){
+    div.className = 'details2';
+    div.id = index;
+    div.innerHTML = `
+    <h2 class="mobile-head">${value.heading}</h2>
+    <h2 class="web-padd">${value.heading2}</h2>
+    <p class="data-arc">${value.text}</p>
+    <div class="programs">
+        <ul class="position-items">
+            <li class="stack-position">html </li>
+            <li class="stack-position">bootstrap </li>
+            <li class="stack-position">Ruby</li>
+        </ul>
+    </div>
+    <button type="button" class="see-project-btn button-hov hov">See Project </button>
+    `
+  }
+
+  work.appendChild(div);
+})
+
+btnClick = document.querySelectorAll('.see-project-btn');
+
+btnClick.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const x = btn.parentNode.id;
+    const pop = document.createElement('div');
+    const overlay = document.getElementById('overlay');
+    pop.className = 'popUp';
+    pop.style.display = 'block';
+    overlay.style.display = 'block';
+    pop.innerHTML = `
+    <div id="top-popup">
+    <h3 class="top-stories">${cardData[x].heading} </h3>
+    <button class="close-button">&times;</button>
+    <div class="programs">
+        <div class="position-buttons">
+            <button class="recentBtn" type="button" id="color">html </button>
+            <button class="recentBtn" type="button" id="color">bootstrap </button>
+            <button class="recentBtn" type="button" id="color">Ruby</button>
+        </div>
+    </div>
+   </div>                    
+<div class="popup-photo">
+<img class="recent-pic" src="images/snapshoot.png" alt="snapshoot Portfolio">
+</div>
+<div class="popup-para">
+<span class="recent-para">
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent
+</span>
+</div>
+<div class="btn">
+<button type="button" class="get-my-resume-btn">See live <a href="#"><img src="images/Icon2g.png" alt="GitHub" class="github"> </button>
+<button type="button" class="get-my-resume-btn">See source  <a href="#"><img src="images/github.png" alt="GitHub" class="github"></a> </button>
+</div>
+    `;
+    work.appendChild(pop);
+  })
+})
+
+document.addEventListener('click', (e) => {
+  const popUp = document.querySelector('.popUp');
+  const target = e.target.closest('.close-button');
+  const overlay = document.querySelector('#overlay');
+  if (target) {
+    popUp.remove();
+    overlay.style.display = 'none';
+    // overlay.classList.add('hidden');
+  }
+});
